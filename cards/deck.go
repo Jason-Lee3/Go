42aa0c5b6b337860961b500a3f5b205e7c007b21
Jason-Lee3 <Jasoneugenelee@gmail.com>
2025-08-25 20:53:29 -0700

    Another commit

--- cards/deck.go
+++ cards/deck.go
@@ -6,8 +6,27 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, cardSuit := range cardSuits {
+		for _, cardValue := range cardValues {
+			cards = append(cards, cardValue+" of "+cardSuit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,10 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Dimaonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
+	// cards.print()
+	hand, remainingCards := deal(cards, 5)
 
-	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	hand.print()
+	remainingCards.print()
 }
