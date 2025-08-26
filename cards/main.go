64cd82421fe052d4b103cfa665cabf58962c6a6c
Jason-Lee3 <Jasoneugenelee@gmail.com>
2025-08-25 23:12:35 -0700

    Write tests

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,23 @@
 package main
 
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 	// cards.print()
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+
+	// temp := cards.toString()
+	// fmt.Println([]byte(temp))
+	// cards.saveToFile("file_1")
+	// cards := newDeckFromFile("file_11")
+	// cards.print()
+
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+
+	// greeting := "Hi There"
+	// fmt.Println([]byte(greeting))
 }
