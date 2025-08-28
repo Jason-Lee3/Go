800ad903afebe6700aa0d40c33d02928c7a717e9
Jason-Lee3 <Jasoneugenelee@gmail.com>
2025-08-27 22:49:34 -0700

    Another commit

---
+++ http/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type logWriter struct{}
+
+func main() {
+	response, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	// bs := make([]byte, 99999) // this way we create a byte slice of size 99999
+	// response.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+
+	io.Copy(lw, response.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
+}
